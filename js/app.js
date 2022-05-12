279f4874b787f7096dd40d1127d5dec902f9ceda
Vladimir Zecevic <zecevicvladimir1@gmail.com>
2022-05-12 15:38:31 +0200

    [ADD] - Homepage - Desktop

---
+++ js/app.js
@@ -0,0 +1,80 @@
+/* #Hamburger
+  ======================================================= */
+const header = document.querySelector('.header');
+const headerBtn = document.querySelector('.header-hamburger');
+
+headerBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  header.classList.toggle('show');
+  body.classList.toggle('no-scroll');
+});
+
+/* #Header
+  ======================================================= */
+
+// Color Change
+if (document.querySelector('.header')) {
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+      header.classList.add('header-scrolled');
+    } else {
+      header.classList.remove('header-scrolled');
+    }
+  });
+}
+
+if (document.querySelector('.header')) {
+  if (window.scrollY > 0) {
+    header.classList.add('header-scrolled');
+  } else {
+    header.classList.remove('header-scrolled');
+  }
+}
+
+/* #Numbers Animation
+  ======================================================= */
+gsap.registerPlugin(ScrollTrigger);
+
+const numbers = document.querySelectorAll('.numbers .num');
+
+ScrollTrigger.create({
+  trigger: ".numbers",
+  onEnter: () => {
+    numbers.forEach((number) => {
+      var zero = {
+        val: 0
+      };
+      var num = number.innerHTML;
+
+      gsap.to(zero, {
+        val: num,
+        duration: 3,
+        scrollTrigger: numbers,
+        onUpdate: function () {
+          number.innerHTML = zero.val.toFixed(0);
+        }
+      });
+    });
+  }
+});
+
+/* #Product Slider
+  ======================================================= */
+if (document.querySelector('.process')) {
+  swiper = new Swiper(".process .swiper", {
+    loop: true,
+    navigation: {
+      nextEl: ".process .next",
+      prevEl: ".process .prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1
+      },
+      1024: {
+        slidesPerView: 2.5
+      }
+    }
+  });
+}
